f389491705e0746e2d80b5b128276281432b9c68
N0H3dy <workmail2776@gmail.com>
2023-02-09 16:09:03 -0500

    Added a the funtion of travers video by transcript

---
+++ Frontend/video-editing.js
@@ -0,0 +1,30 @@
+// This script deals with adding functionality to the transcript, and all other features on the site
+var transcript = document.getElementById("transcript");
+
+var lines = transcript.getElementsByTagName("p");
+
+for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    var time = line.getAttribute("data-time");
+
+    // Add a click event listener to the line
+    line.addEventListener("click", function() {
+    var video = document.getElementById("video");
+
+    // Set the video's current time to the time attribute
+    video.currentTime = this.getAttribute("data-time");
+    video.play();
+    })
+}
+    
+document.getElementById("play-button").addEventListener("click", function() {
+// Code to play the video goes here
+});
+
+document.getElementById("pause-button").addEventListener("click", function() {
+// Code to pause the video goes here
+});
+
+document.getElementById("stop-button").addEventListener("click", function() {
+// Code to stop the video goes here
+});
