6a2e9e426b996ed7e6e285a3a6e627da6fdb54dd
N0H3dy <workmail2776@gmail.com>
2023-02-11 22:13:00 -0500

    Added the ability to upload videos

--- Frontend/upload-video.js
+++ Frontend/upload-video.js
@@ -2,6 +2,7 @@ const form = document.querySelector("form"),
 fileInput = document.querySelector(".file-input"),
 progressArea = document.querySelector(".progress-area"),
 uploadedArea = document.querySelector(".uploaded-area");
+video = document.querySelector(".video-feild");
 
 // form click event
 form.addEventListener("click", () =>{
@@ -11,11 +12,13 @@ form.addEventListener("click", () =>{
 fileInput.onchange = ({target})=>{
   let file = target.files[0]; //getting file [0] this means if user has selected multiple files then get first one only
   if(file){
+    console.log(file);
     let fileName = file.name; //getting file name
     if(fileName.length >= 12){ //if file name length is greater than 12 then split it and add ...
       let splitName = fileName.split('.');
       fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
     }
+    console.log(file);
     uploadFile(fileName); //calling uploadFile with passing file name as an argument
   }
 }
@@ -23,7 +26,7 @@ fileInput.onchange = ({target})=>{
 // file upload function
 function uploadFile(name){
   let xhr = new XMLHttpRequest(); //creating new xhr object (AJAX)
-  xhr.open("POST", "upload.php"); //sending post request to the specified URL
+  xhr.open("POST", "php/upload.php"); //sending post request to the specified URL
   xhr.upload.addEventListener("progress", ({loaded, total}) =>{ //file uploading progress event
     let fileLoaded = Math.floor((loaded / total) * 100);  //getting percentage of loaded file size
     let fileTotal = Math.floor(total / 1000); //gettting total file size in KB from bytes
@@ -64,5 +67,8 @@ function uploadFile(name){
     }
   });
   let data = new FormData(form); //FormData is an object to easily send form data
+  console.log(data.values());
   xhr.send(data); //sending form data
+
+  
 }
